7555648dc8c57dda6d23f985e8e3bc6019bb6000
Eduard Aleksandrov <eduard.ale@list.ru>
2025-06-22 15:04:04 +0300

    test5 malloc

--- test5/main.c
+++ test5/main.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <omp.h>
+#include <stdlib.h>
 
-void vector_addition(float *a, float *b, float *result, int n) {
+void vector_addition(float *a, float *b, float *result, long long int n) {
     #pragma omp simd
-    for (int i = 0; i < n; i++) {
+    for (long long int i = 0; i < n; i++) {
         result[i] = a[i] + b[i];
     }
 }
 
 int main() {
-    int n = 1000;
-    float a[1000], b[1000], result[1000];
+    long long int n = 1500000000;
+    float* a = (float*) malloc(sizeof(float) * n);
+    float* b = (float*) malloc(sizeof(float) * n);
+    float* result = (float*) malloc(sizeof(float) * n);
 
     // Инициализация массивов
-    for (int i = 0; i < n; i++) {
+    for (long long int i = 0; i < n; i++) {
         a[i] = i * 1.0f;
         b[i] = i * 2.0f;
     }
@@ -21,7 +24,7 @@ int main() {
     vector_addition(a, b, result, n);
 
     // Вывод результата
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 10; i++) {
         printf("result[%d] = %f\n", i, result[i]);
     }
 
